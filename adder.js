1036c9b8de40806407d15683e0537135149d0161
Dan Rosen <drosen84@gmail.com>
2017-12-22 11:00:59 -0600

    starter files

--- adder.js
+++ adder.js
@@ -1,9 +1,12 @@
+'use strict';
+
+
 function adder(a, b) {
-  // validate that both inputs are numbers
-  if (!(typeof a === 'number' && typeof b === 'number')) {
-    throw Error('Oh no!');
-  }
-  return a + b;
+    // validate that both inputs are numbers
+    if (!(typeof a === 'number' && typeof b === 'number')) {
+        throw Error('Oh no!');
+    }
+    return a + b;
 }
 
 module.exports = adder;
