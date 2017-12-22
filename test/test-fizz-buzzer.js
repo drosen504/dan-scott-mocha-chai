0a932e4832cbc044608df63c38fd06430d8d4edb
A. Scott Tribble <scott.tribble@As-MacBook-Pro.local>
2017-12-22 11:54:03 -0800

    added bad Input test

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -8,25 +8,43 @@ const fizzBuzzer = require('../fizzBuzzer');
 // unit tests for our `fizzBuzzer` function
 describe('fizzBuzzer', function() {
     
-    it('should provide fizz/buzz/fizz-buzz or a number', function() {
-        const normalCases = [
-            {num: 30, expected: 'fizz-buzz'},
-            {num: 10, expected: 'buzz'},
-            {num: 6, expected: 'fizz'},
-            {num: 2, expected: 2}
-        ];
+  it('should provide fizz/buzz/fizz-buzz or a number', function() {
+    const normalCases = [
+      {num: 30, expected: 'fizz-buzz'},
+      {num: 10, expected: 'buzz'},
+      {num: 6, expected: 'fizz'},
+      {num: 2, expected: 2}
+    ];
 
-        normalCases.forEach(function(input) {
-            const answer = fizzBuzzer(input.num);
-            answer.should.equal(input.expected);
-        });
-
-
-
-
+    normalCases.forEach(function(input) {
+      const answer = fizzBuzzer(input.num);
+      answer.should.equal(input.expected);
     });
-});
 
+
+
+
+  });
+
+  it('should raise an error if argument is not a number', function(){
+    const badInputs = 
+      ['arr', true, '3'];
+      //  function(badInputs){
+      //    fizzBuzzer(badInputs).should.throw(Error);
+      //  };   
+    badInputs.forEach(function(input){
+      (function(){
+        (fizzBuzzer(input)).should.be(Error);
+      });
+    });
+  });
+});
+//   badInputs.forEach(function(input) {
+// (function () {
+//     adder(input[0], input[1]);
+// }).should.throw(Error);
+//         });
+// });
 
 // const fizzBuzzInputs = [15, 30, 45];
 // fizzBuzzInputs.forEach(function(input) {
